081d71137da2a8bfc25567b4382051a74e73e68b
Kuppuch <kuppuchr@gmail.com>
2020-06-23 20:49:38 +0300

    Build scripts added

---
+++ ClientBuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd GRPCGreeterClient/GRPCGreeterClient;
+pwd;
+ls;
+dotnet build
+exit 0;
---
+++ SeverBuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd GrpcService1/GrpcService1;
+pwd;
+ls;
+dotnet build
+exit 0;
